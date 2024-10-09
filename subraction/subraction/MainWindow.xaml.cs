598937d478032e05a61cc5e77b4dbe973e33eae7
renuga <121298131+Renugamanikandan@users.noreply.github.com>
2024-10-09 15:40:49 +0530

    #

---
+++ subraction/subraction/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace subraction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int value1 = Convert.ToInt32(txtvalue1.Text);
+        int value2 = Convert.ToInt32(txtvalue2.Text);
+        int result = value1 + value2;
+        MessageBox.Show(Convert.ToString(result));
+private void btnresult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
